f966cb360fba2962592f6cf71a2e03e43cb3dafe
joseph <jconrow00@gmail.com>
2024-02-16 21:37:14 -0800

    first NAOiq

--- PythonParser/main.py
+++ PythonParser/main.py
@@ -1,24 +1,27 @@
-# This is a sample Python script.
+
+from __future__ import print_function
 import fileinput
+import decimal
+import fileinput
+import csv
+import naoqi
+from contextlib import closing
 
 # Press Shiffor line in fileinput.input(encoding="utf-8"):
 # t+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import torch
-import decimal
-import fileinput
-import csv
-from transformers import pipeline, DistilBertTokenizer, DistilBertForSequenceClassification, TextClassificationPipeline
+
+#import torch
+#from transformers import pipeline, DistilBertTokenizer, DistilBertForSequenceClassification, TextClassificationPipeline
 
 
+#model_id = "lxyuan/distilbert-base-multilingual-cased-sentiments-student"
 
-model_id = "lxyuan/distilbert-base-multilingual-cased-sentiments-student"
 
-
-def analyze_sentiment(text):
-    sentiment_analyzer = pipeline("sentiment-analysis", model=model_id, top_k=None)
-    result = sentiment_analyzer(text)
-    return result[0]['label'], result[0]['score']
+#def analyze_sentiment(text):
+#    sentiment_analyzer = pipeline("sentiment-analysis", model=model_id, top_k=None)
+#    result = sentiment_analyzer(text)
+#    return result[0]['label'], result[0]['score']
 
 
 class ScriptLine:
@@ -28,74 +31,41 @@ class ScriptLine:
         self.gestures = gestures
 
     def __str__(self):
-        string = (f"\033[91mVoice: {self.voice} "
-                  f"\033[0m| \033[92mText: \"{self.text}\" "
-                  f"\033[0m| \033[93mGestures: ")
+        string = ('\033[91mVoice: {} \033[0m| \033[92mText: "{}" \033[0m| \033[93mGestures: '
+                  .format(self.voice, self.text))
         for i in range(len(self.gestures)):
             if i < len(self.gestures) - 1:
-                string += f"{self.gestures[i]}, "
+                string += self.gestures[i]
+                string += ', '
             else:
-                string += f"{self.gestures[i]}"
-        #string += f"\033[0m"
+                string += self.gestures[i]
         return string
 
     def help_csv(self):
-        array = []
-        array.append(f"{self.voice}")
-        array.append(f"{self.text}")
+        array = [self.voice, self.text]
         for i in range(len(self.gestures)):
-            array.append(f"{self.gestures[i]}")
+            array.append(self.gestures[i])
         return array
-    def append_to_csv(self,filename):
+
+    def append_to_csv(self, filename):
         with open(filename, 'a') as f:
             writer = csv.writer(f)
             writer.writerow(self.help_csv())
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
 
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def add_node(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def print(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
-
-
-def create_linked_list(input_string):
-    words = input_string.split()
-    linked_list = LinkedList()
-    for word in words:
-        linked_list.add_node(word)
-    return linked_list
-
-
-def check_speed(line_number, line):
-    # extracts the speed number (1-5) before the '#'
-    line_speed = line.partition('#')[0]
-    # checks if the speed number is valid, else error
-    if not line_speed.isdigit():
-        print(f"\033[1:91mIn Script line({line_number}): speed (prior to '#') is snot a positive integer\033[0m")
-        exit(1)
-    else:
-        line_speed = int(line_speed)
-    if line_speed < 1 or line_speed > 5:
-        print(f"\033[1:91mIn Script line({line_number}): speed out of range: {line_speed}\033[0m")
-        exit(1)
-    return line_speed
+# def check_speed(line_number, line):
+#    # extracts the speed number (1-5) before the '#'
+#    line_speed = line.partition('#')[0]
+#    # checks if the speed number is valid, else error
+#    if not line_speed.isdigit():
+#        print(f"\033[1:91mIn Script line({line_number}): speed (prior to '#') is snot a positive integer\033[0m")
+#        exit(1)
+#    else:
+#        line_speed = int(line_speed)
+#    if line_speed < 1 or line_speed > 5:
+#        print(f"\033[1:91mIn Script line({line_number}): speed out of range: {line_speed}\033[0m")
+#        exit(1)
+#    return line_speed
 
 
 # RETURNS an int of the voice
@@ -137,44 +107,46 @@ def extract_text(line):
     return text
 
 
-def print_line_sentiments(line_number, sentiment_file_results):
-    # prints out these line-sentiments
-    for i in range(3):
-        current_sentiment_label = sentiment_file_results[line_number][0][i]['label'].upper()
-        # labels the color to the sentiment
-        if current_sentiment_label == 'POSITIVE':
-            print(f"\033[92m", end='')
-        elif current_sentiment_label == 'NEGATIVE':
-            print(f"\033[91m", end='')
-        elif current_sentiment_label == 'NEUTRAL':
-            print(f"\033[93m", end='')
-        print(f"\t{current_sentiment_label}", end='')
-        print(f"\033[0m \t{round(sentiment_file_results[line_number][0][i]['score'] * 100, 2)}%")
-    return
+#def print_line_sentiments(line_number, sentiment_file_results):
+#    # prints out these line-sentiments
+#    for i in range(3):
+#        current_sentiment_label = sentiment_file_results[line_number][0][i]['label'].upper()
+#        # labels the color to the sentiment
+#        if current_sentiment_label == 'POSITIVE':
+#            print('\033[92m', )
+#        elif current_sentiment_label == 'NEGATIVE':
+#            print('\033[91m')
+#        elif current_sentiment_label == 'NEUTRAL':
+#            print('\033[93m')
+#        print('\t',current_sentiment_label,)
+#        print('\033[0m \t', round(sentiment_file_results[line_number][0][i]['score'] * 100, 2), '%')
+#    return
 
 
 def main():
     # receive text input from script file
-    with fileinput.input(files='InputScript.txt') as input_script:
-        with open(r"InputScript.txt", 'r') as fp:
-            lines = len(fp.readlines())
-        line_results = [lines]
+    input_script = fileinput.input(files='InputScript.txt')
 
+    fp = open(r'InputScript.txt', 'r')
+    lines = len(fp.readlines())
+    line_results = [lines]
+    fp.close()
 
-        # goes through each line of file
-        for line in input_script:
-            line_number = fileinput.lineno()
-            # returns a str array of gestures
+    # goes through each line of file
+    for line in input_script:
+        #line_number = fileinput.lineno()
+        voice = extract_voice(line)
+        text = extract_text(line)
+        gestures = extract_gesture(line)
+        testing_class = ScriptLine(voice, text, gestures)
+        print(testing_class)
+        testing_class.append_to_csv('OutputScript.csv')
+    fileinput.close()
 
-            voice = extract_voice(line)
-            text = extract_text(line)
-            gestures = extract_gesture(line)
-            testing_class = ScriptLine(voice, text, gestures)
-            print(testing_class)
-            testing_class.append_to_csv(    "OutputScript.csv")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

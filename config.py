f0ef88c1c959f4b6d416527142a0a013b0a0cf0e
joseph <jconrow00@gmail.com>
2024-06-04 18:58:48 -0700

    please_work_now

---
+++ config.py
@@ -0,0 +1,11 @@
+# Aimed towrards physcial Pepper (below)
+PEPPER_IP = '192.168.50.155'
+PEPPER_PORT = 9559
+# Aimed towards virtual Pepper (below)
+# PEPPER_IP = 'localhost'
+# PEPPER_PORT = 33361
+
+INPUT_FILE = '../inputs/MultiMeditationEnglish.txt'
+# SPEAKER_WAV = None        # use this value if no multi-lingual voice is used
+SPEAKER_WAV = '../inputs/female.wav'
+
